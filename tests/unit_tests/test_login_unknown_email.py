43798b4a19ca56ef2cd2d9df44185a62127cbae1
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-16 22:10:19 +0200

    tests/unit_tests/test_login_unknown_email.py + error locustfile.py

--- tests/unit_tests/test_login_unknown_email.py
+++ tests/unit_tests/test_login_unknown_email.py
@@ -7,9 +7,13 @@ class TestLogin:
     client = app.test_client()
 
     def test_valid_email(self):
-        result = self.client.post("/showSummary", data=dict(email="admin@irontemple.com"))
+        result = self.client.post("/showSummary", data={"email": server.clubs[0]["email"]})
         assert result.status_code == 200
 
     def test_invalid_email(self):
-        result = self.client.post("/showSummary", data=dict(email="jhbdfkshdvf"))
+        result = self.client.post("/showSummary", data=dict(email="test_invalid_email"))
+        assert result.status_code == 403
+
+    def test_empty_email(self):
+        result = self.client.post("/showSummary", data={"email":" "})
         assert result.status_code == 403
