65eafe04ee078aa0f88cb9fc5868bb993516f865
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-18 19:35:37 +0200

    def showSummary() more fluid organization

--- server.py
+++ server.py
@@ -1,17 +1,6 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
-
-
-def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
-
-
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+from utils import loadClubs, loadCompetitions
 
 
 app = Flask(__name__)
@@ -27,9 +16,13 @@ def index():
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     try:
-        club = [club for club in clubs if club['email'] == request.form['email']][0]
-        return render_template('welcome.html', club=club, competitions=competitions)
-    except IndexError:
+        club_list = []
+        for i in clubs:
+            if i['email'] == request.form['email']:
+                club_list.append(i)
+        email_club = list[0]
+        return render_template('welcome.html', club=email_club, competitions=competitions)
+    except:
         if request.form['email'] == '':
             flash("Please enter your email.")
         else:
